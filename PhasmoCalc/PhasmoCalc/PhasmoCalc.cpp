cf510a9474da0faf78f562ca19fe1c4a54b39671
FGBxRamel <kretschmer.raziel@gmail.com>
2021-08-18 13:53:10 +0200

    Set up JSON and the json file

---
+++ PhasmoCalc/PhasmoCalc/PhasmoCalc.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+using namespace std;
+using json = nlohmann::json;
+
+
+int main()
+{
+	string json_path = "ghosts.json";
+	json ghosts;
+	fstream ghost_file(json_path);
+	
+	if (ghost_file)
+	{
+		ghost_file >> ghosts;
+		ghost_file.close();
+	}
+	else
+	{
+		ghosts = {
+			{"Spirit", 0},
+			{"Gespenst", 0},
+			{"Phantom", 0},
+			{"Poltergeist", 0},
+			{"Banshee", 0},
+			{"Dschinn", 0},
+			{"Mare", 0},
+			{"Revenant", 0},
+			{"Shade", 0},
+			{"Demon", 0},
+			{"Yurei", 0},
+			{"Oni", 0},
+			{"Yokai", 0},
+			{"Hantu", 0}
+		};
+		//ofstream ghost_file(json_path);
+		ghost_file << ghosts;
+		ghost_file.close();
+	}
+	while (true)
+	{
+		
+	}
+	return 1;
+}
+
